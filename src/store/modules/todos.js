d69c12e847bc7b40de971129c6e58db4ca79e932
Steve_Zhang <wentaozhang0305@gmail.com>
2020-05-15 14:07:34 -0700

    setup vuex

---
+++ src/store/modules/todos.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const state = {
+    todos: [
+        {
+            id: 1,
+            titile: 'Todo one'
+        },
+        {
+            id: 2,
+            title: 'Todo Two'
+        }
+    ]
+
+};
+
+const getters = {};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+}
